d1fa8c16c6e19b7c4e44ebc54f6414265f72a384
Thang0226 <thang.nd0226@gmail.com>
2024-10-17 16:59:09 +0700

    first commit

---
+++ src/model/ComparableCircle.java
@@ -0,0 +1,20 @@
+package model;
+
+public class ComparableCircle extends Circle
+		implements Comparable<ComparableCircle> {
+	public ComparableCircle() {
+	}
+
+	public ComparableCircle(double radius) {
+		super(radius);
+	}
+
+	public ComparableCircle(double radius, String color, boolean filled) {
+		super(radius, color, filled);
+	}
+
+	@Override
+	public int compareTo(ComparableCircle o) {
+		return Double.compare(super.getRadius(), o.getRadius());
+	}
+}
